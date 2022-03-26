2ed6d47bb1980ffdef3a980d10dfbce51797e57d
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-03-26 17:40:22 -0600

    F Checks app version and build number from given bundle

---
+++ WhatsNew/Check Version Feature/Bundle+BuildNumber.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2022 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public extension Bundle {
+    var releaseVersionNumber: String? {
+        infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+
+    var buildVersionNumber: String? {
+        infoDictionary?["CFBundleVersion"] as? String
+    }
+
+    var releaseVersionNumberPretty: String {
+        "v\(releaseVersionNumber ?? "1.0.0")"
+    }
+}
---
+++ WhatsNew/WhatsNewEnvironment.swift
@@ -0,0 +1,7 @@
+//
+//  Copyright © 2022 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+class WhatsNewEnvironment {}
