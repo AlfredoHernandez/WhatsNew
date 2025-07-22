559d8b2724db7bd410480210af83de5d17648420
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2025-07-22 17:54:38 -0600

    🔖 Release v1.0.0 (#10)
    * [ALF-20] Update package to Swift 6
    
    * [ALF-20] Update sample app project to Swift 6
    
    * Update gh action
    
    * Fix typo

--- Sources/WhatsNew/Composition/WhatsNewComposer.swift
+++ Sources/WhatsNew/Composition/WhatsNewComposer.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright © 2023 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//  Copyright © 2025 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
 import Foundation
@@ -8,12 +8,13 @@ struct WhatsNewState: Equatable {
     var title: String
     var features: [WhatsNewItemViewModel]
 
-    public init(title: String, features: [WhatsNewItemViewModel]) {
+    init(title: String, features: [WhatsNewItemViewModel]) {
         self.title = title
         self.features = features
     }
 }
 
+@MainActor
 public func WhatsNewView(title: String, features: FeaturesBuilder, didTapContinueButton: @escaping () -> Void) -> WhatsNew {
     WhatsNew(
         viewModel: WhatsNewViewModel(
